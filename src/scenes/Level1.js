66b0d7dbefbf3708f1577c70a037c8eceb97b9de
Debora-k <debora0104k@gmail.com>
2024-12-19 20:37:47 -0700

    added flour and eggs interactions

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -19,20 +19,54 @@ export default class Level1 extends Phaser.Scene {
 	/** @returns {void} */
 	editorCreate() {
 		const textStyle = {"fontFamily": "PixelifySans-Regular", "fontSize": "23px", "color":"black", "align": "left", wordWrap:{width:220, useAdvancedWrap:true}};
+		
+		
 		const level_bg = this.add.image(640, 360, "level_bg");
 		level_bg.setScale(0.5, 0.5);
 		level_bg.setInteractive(new Phaser.Geom.Rectangle(0, 0, 250, 250), Phaser.Geom.Rectangle.Contains);
 
+
+		const chopping_board = this.add.image(700, 450, "chopping_board");
+		chopping_board.setScale(0.24, 0.24);
+
+		let flour = this.add.image(720, 450, "flour");
+		flour.setScale(0.8, 0.8);
+		flour.setVisible(false);
+		const flour_jar = this.add.image(165, 410, "flour_jar");
+		flour_jar.setScale(0.12, 0.12);
+		flour_jar.setInteractive().on('pointerdown', () => {
+			flour.setVisible(true);
+		});
+
+		const flour_egg_1 = this.add.image(720, 450, "flour_egg_1");
+		flour_egg_1.setScale(0.8, 0.8);
+		flour_egg_1.setVisible(false);
+		
+		const flour_egg_2 = this.add.image(720, 450, "flour_egg_2");
+		flour_egg_2.setScale(0.8, 0.8);
+		flour_egg_2.setVisible(false);
+
+		const egg_outline = this.add.image(746, 450, "egg_outline");
+		egg_outline.setScale(0.04, 0.04);
+		egg_outline.setVisible(false);
 		const eggs_1 = this.add.image(150, 150, "eggs_1");
 		eggs_1.setScale(0.13, 0.13);
-		eggs_1.setInteractive().on('pointerup', () => {});
+		let eggs_clickedCnt = 0;
+		eggs_1.setInteractive().on('pointerdown', () => {
+			eggs_clickedCnt++;
+			flour_egg_1.setVisible(true);
+			if(eggs_clickedCnt === 1) {
+				egg_outline.setVisible(true);
+			};
+			if (eggs_clickedCnt === 2) {
+				egg_outline.setVisible(false);
+				flour_egg_2.setVisible(true);
+			}
+		});
 
 		const olive_oil = this.add.image(380, 100, "olive_oil");
 		olive_oil.setScale(0.08, 0.08);
 		olive_oil.setInteractive().on('pointerup', () => {});
-
-		const tissues = this.add.image(570, 100, "tissues");
-		tissues.setScale(0.1, 0.1);
 
 		const knife = this.add.image(820, 180, "knife");
 		knife.setScale(0.3, 0.3);
@@ -42,15 +76,15 @@ export default class Level1 extends Phaser.Scene {
 		semolina_flour_jar.setScale(0.09, 0.09);
 		semolina_flour_jar.setInteractive().on('pointerup', () => {});
 
+		const napkin = this.add.image(125, 650, "napkin");
+		napkin.setScale(0.2, 0.2);
+
+		const tissues = this.add.image(570, 100, "tissues");
+		tissues.setScale(0.1, 0.1);
+
 		const whisk = this.add.image(440, 495, "whisk");
 		whisk.setScale(0.3, 0.3);
 		whisk.setInteractive().on('pointerup', () => {});
-
-		const chopping_board = this.add.image(700, 450, "chopping_board");
-		chopping_board.setScale(0.24, 0.24);
-
-		const napkin = this.add.image(125, 650, "napkin");
-		napkin.setScale(0.2, 0.2);
 
 		const tomatoes = this.add.image(115, 615, "tomatoes");
 		tomatoes.setScale(0.095, 0.095);
@@ -60,13 +94,9 @@ export default class Level1 extends Phaser.Scene {
 		basil.setScale(0.1, 0.1);
 		basil.setInteractive().on('pointerup', () => {});
 
-		const flour_jar = this.add.image(165, 410, "flour_jar");
-		flour_jar.setScale(0.12, 0.12);
-		flour_jar.setInteractive().on('pointerup', () => {});
 
 		const clipboard = this.add.image(1090, 430, "clipboard");
 		clipboard.setScale(0.25, 0.25);
-		
 		const rect1 = this.add.rectangle(985, 330, 20, 20, 0x808080, 1);
 
 		const clipboard_list1 = this.add.text(1000, 330, "Add 100g flour", {});
@@ -99,17 +129,18 @@ export default class Level1 extends Phaser.Scene {
 		clipboard_list6.setStyle(textStyle);
 
 		this.level_bg = level_bg;
+		this.chopping_board = chopping_board;
 		this.eggs_1 = eggs_1;
+		this.flour_egg_1 = flour_egg_1;
 		this.olive_oil = olive_oil;
 		this.tissues = tissues;
 		this.knife = knife;
 		this.semolina_flour_jar = semolina_flour_jar;
 		this.whisk = whisk;
-		this.chopping_board = chopping_board;
 		this.napkin = napkin;
+		this.flour_jar = flour_jar;
 		this.tomatoes = tomatoes;
 		this.basil = basil;
-		this.flour_jar = flour_jar;
 		this.clipboard = clipboard;
 		this.rect1 = rect1;
 		this.rect2 = rect2;
@@ -117,6 +148,8 @@ export default class Level1 extends Phaser.Scene {
 		this.rect4 = rect4;
 		this.rect5 = rect5;
 		this.rect6 = rect6;
+
+		this.flour = flour;
 
 
 		this.events.emit("scene-awake");
@@ -148,7 +181,10 @@ export default class Level1 extends Phaser.Scene {
 	flour_jar;
 	/** @type {Phaser.GameObjects.Image} */
 	clipboard;
-
+	/** @type {Phaser.GameObjects.Image} */
+	flour;
+	/** @type {Phaser.GameObjects.Image} */
+	flour_egg_1;
 
 
 	/* START-USER-CODE */
